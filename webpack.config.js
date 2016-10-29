e8c5cee020403457ccea1212057b93c89efc7e13
Glen Mailer <glenjamin@gmail.com>
2016-10-29 17:07:47 +0100

    Upgrade packages, linting works so far

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,22 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'eval',
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    './client/index'
-  ],
+  devtool: 'eval-source-map',
+  entry: './client/index',
   output: {
     path: path.join(__dirname, 'build'),
     filename: 'bundle.js',
     publicPath: '/client/'
   },
   plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
   module: {
     loaders: [
       { test: /\.js$/,
-        loaders: ['react-hot', 'babel'],
+        loader: 'babel',
         include: path.join(__dirname, 'client')
       },
       { test: /\.css$/,
